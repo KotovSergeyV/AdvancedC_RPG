956e601d180643ce868d158dbfb9f9481d68c6a8
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2025-03-15 19:35:41 +0300

    Damage System v0.1
    I_damagable
    Struct damage info

---
+++ Assets/DamageSystem/I_Damage.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface I_Damage
+{
+    int Damage(GameObject target, Struct_DamageData damageData) { return 0; }
+}
---
+++ Assets/DamageSystem/Struct_DamageData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public struct Struct_DamageData
+{
+    int Damage;
+    Enum_DamageResponses Responce; 
+    Enum_DamageTypes DamageType;
+
+    bool isInneviåtable;
+    bool isBlockable;
+}
