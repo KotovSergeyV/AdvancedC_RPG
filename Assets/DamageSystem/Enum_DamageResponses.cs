872c1860ff1692a709da3f009e02af9d4c33729d
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2025-03-16 14:56:09 +0300

    Health system + damage system merge (#1)
    * Health BaseRealisation
    
    -interface Health
    -Health component
    -No tests
    
    * Damage System v0.1
    
    I_damagable
    Struct damage info
    
    * DamageSystemBasic
    
    -Basic damage system
    -Basic responses
    -Tested
    - Future implementation need to be discussed

---
+++ Assets/DamageSystem/Enum_DamageResponses.cs
@@ -0,0 +1,9 @@
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+public enum Enum_DamageResponses
+{
+    NoResponse,
+    SmallStun,
+    Stun
+}
---
+++ Assets/DamageSystem/Struct_DamageData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public struct Struct_DamageData
+{
+    public int DamageAmount;
+    public Enum_DamageResponses Responce;
+    public Enum_DamageTypes DamageType;
+
+    public bool isInneviåtable;
+    public bool isBlockable;
+
+}
