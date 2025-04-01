8ea8c5413fcca1cd49b9b836d31532426c5ff23b
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2025-04-01 11:28:59 +0300

    Lab 1 (#5)
    * All changes from soft reset
    
    * Data from branch dev
    
    * Restoration commit
    
    - Major Issues from repo changes solved.
    - Minor range weapon bug solved.
    - Folder "developers folder" in assets created
    
    * Restoration commit
    
    - Major Issues from repo changes solved.
    - Minor range weapon bug solved.
    - Folder "developers folder" in assets created
    
    * Folder restructurisation
    
    
    **Next stage**
    Redo architecture
    Bootstrapper

---
+++ Assets/Developers Folder/Scripts/Characters/AnimatorController.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour, IAnimatorController
+{
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void PlayWalkAnimation(bool state)
+    {
+        _animator.SetBool("isWalking", state);
+    }
+
+    public void PlayRunAnimation(bool state)
+    {
+        _animator.SetBool("isRunning", state);
+    }
+
+    public void PlayAttackAnimation()
+    {
+        _animator.SetInteger("AttackClip", Random.Range(1, 4));
+    }
+
+    public void SetFloatToAnimation(float x, float y)
+    {
+        _animator.SetFloat("X", x);
+        _animator.SetFloat("Y", y);
+    }
+
+    public void SetAttackClipToZero()
+    {
+        _animator.SetInteger("AttackClip", 0);
+    }
+
+    public void PlayAttackAnimationByTrigger()
+    {
+        _animator.SetTrigger("AttackTrigger");
+    }
+
+    public void PlayJumpAnimation()
+    {
+        _animator.SetTrigger("Jump");
+    }
+
+    public void PlayDeathAnimation(bool state)
+    {
+        _animator.SetBool("isDead", state);
+    }
+
+    public void PlayHitAnimation()
+    {
+        _animator.SetTrigger("Hit");
+    }
+}
---
+++ Assets/Developers Folder/Scripts/Components/DamageSystem/DamageDeallerComponent.cs
@@ -0,0 +1,94 @@
+using Unity.VisualScripting;
+using UnityEditor.Playables;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class DamageDeallerComponent : MonoBehaviour, I_DamageDealler
+{
+    public int Damage(GameObject target, Struct_DamageData damageData, I_Stat ownerStats=null) 
+    {
+        if (CheckNull(target))
+        { 
+            Debug.Log("No target");
+            return 0;
+        }
+
+        if (ownerStats != null) { damageData = CalaculatePotentialDamage(ownerStats, damageData); }
+
+
+        I_Stat targetObject = target.GetComponent<I_Stat>();
+        if (CheckNull(targetObject))
+        {
+            Debug.Log("No I_Stat");
+            return 0;
+        }
+
+        int luck = targetObject.GetLuck();
+        int defence = targetObject.GetDefence();
+        int agility = targetObject.GetAgility();
+
+        if (!damageData.isInneviåtable)
+        {
+            int baseDodgeChance = 5;
+            float dodgeChance = (float)(baseDodgeChance + (agility * 0.1) + (luck * 0.05));
+
+            if (dodgeChance >= Random.Range(1, 100))
+            {
+                Debug.Log("Dodged");
+                return 0;
+            }
+
+            else if (target.GetComponent<I_EntityStates>().GetEntityState() == Enum_EntityStates.Blocking && damageData.isBlockable)
+            {
+                SetTargetState(target.GetComponent<I_EntityStates>(), damageData.Responce);
+                Debug.Log("DamagedBlocked");
+                return ApplyDamage(target.GetComponent<I_Health>(), defence, damageData.DamageAmount * 0.1f);
+            }
+
+        }
+
+        SetTargetState(target.GetComponent<I_EntityStates>(), damageData.Responce);
+        Debug.Log("DamagedFull: "+ damageData.DamageAmount);
+        return ApplyDamage(target.GetComponent<I_Health>(), defence, damageData.DamageAmount);
+
+
+    }
+
+    private int ApplyDamage(I_Health target, int targetsDefence, float damage)
+    {
+        if (CheckNull(target)) { return 0; }
+
+        int summarisedDamage = (int)(damage - (targetsDefence * 0.5f));
+        summarisedDamage = Mathf.Max(summarisedDamage, 0);
+        Debug.Log(target.Damage(summarisedDamage));
+        return target.Damage(summarisedDamage);
+    }
+
+
+    private bool CheckNull(object target) { return target == null; }
+
+
+    private void SetTargetState(I_EntityStates target, Enum_DamageResponses response)
+    {
+        if (CheckNull(target)) { return; }
+        switch (response)
+        {
+            case Enum_DamageResponses.SmallStun:
+                target.SetEntityState(Enum_EntityStates.SmallStunned);
+                break;
+
+            case Enum_DamageResponses.Stun:
+                target.SetEntityState(Enum_EntityStates.Stunned);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private Struct_DamageData CalaculatePotentialDamage(I_Stat weaponHolderStats, Struct_DamageData wDamageData)
+    {
+        wDamageData.DamageAmount += weaponHolderStats.GetAttack();
+        return wDamageData;
+    }
+}
